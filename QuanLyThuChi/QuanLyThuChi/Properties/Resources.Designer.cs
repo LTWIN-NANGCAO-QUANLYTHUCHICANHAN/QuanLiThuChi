2b1ac2bca96ac10ce0f5fea84db772a8ea9a8dbb
thanhkhoatran <35222306+thanhkhoatran@users.noreply.github.com>
2018-09-30 15:32:33 +0700

    Revert "frm-PhieuThu"
    This reverts commit 6e985bdeb7a8edae972bffd9cbfad878ccf7ff3f.

--- QuanLyThuChi/QuanLyThuChi/Properties/Resources.Designer.cs
+++ QuanLyThuChi/QuanLyThuChi/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace QuanLyThuChi.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap _05_info_512 {
+            get {
+                object obj = ResourceManager.GetObject("05_info-512", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap _23_512 {
             get {
                 object obj = ResourceManager.GetObject("23-512", resourceCulture);
@@ -103,29 +113,9 @@ namespace QuanLyThuChi.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap _678092_sign_add_512 {
+        internal static System.Drawing.Bitmap _9_avatar_512 {
             get {
-                object obj = ResourceManager.GetObject("678092-sign-add-512", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap add {
-            get {
-                object obj = ResourceManager.GetObject("add", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap add1 {
-            get {
-                object obj = ResourceManager.GetObject("add1", resourceCulture);
+                object obj = ResourceManager.GetObject("9_avatar-512", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -163,6 +153,16 @@ namespace QuanLyThuChi.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap business_15_512 {
+            get {
+                object obj = ResourceManager.GetObject("business-15-512", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Button_Log_Off_icon {
             get {
                 object obj = ResourceManager.GetObject("Button-Log-Off-icon", resourceCulture);
@@ -173,9 +173,9 @@ namespace QuanLyThuChi.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap delete {
+        internal static System.Drawing.Bitmap Coin_Hand_512 {
             get {
-                object obj = ResourceManager.GetObject("delete", resourceCulture);
+                object obj = ResourceManager.GetObject("Coin-Hand-512", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -233,6 +233,16 @@ namespace QuanLyThuChi.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Door_Exit_Sign_Out_512 {
+            get {
+                object obj = ResourceManager.GetObject("Door-Exit-Sign-Out-512", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap dsthuchi {
             get {
                 object obj = ResourceManager.GetObject("dsthuchi", resourceCulture);
@@ -273,9 +283,9 @@ namespace QuanLyThuChi.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap if_sign_add_299068 {
+        internal static System.Drawing.Bitmap if_Information_31216 {
             get {
-                object obj = ResourceManager.GetObject("if_sign-add_299068", resourceCulture);
+                object obj = ResourceManager.GetObject("if_Information_31216", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -286,6 +296,26 @@ namespace QuanLyThuChi.Properties {
         internal static System.Drawing.Bitmap income {
             get {
                 object obj = ResourceManager.GetObject("income", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap key_512 {
+            get {
+                object obj = ResourceManager.GetObject("key-512", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Line_Icons_Misc_NEW_22_512 {
+            get {
+                object obj = ResourceManager.GetObject("Line_Icons_Misc_NEW-22-512", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -306,6 +336,46 @@ namespace QuanLyThuChi.Properties {
         internal static System.Drawing.Bitmap Money_512 {
             get {
                 object obj = ResourceManager.GetObject("Money-512", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Money_Bag_512 {
+            get {
+                object obj = ResourceManager.GetObject("Money-Bag-512", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Money_Bag_512__1_ {
+            get {
+                object obj = ResourceManager.GetObject("Money-Bag-512 (1)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap money_bag_512__2_ {
+            get {
+                object obj = ResourceManager.GetObject("money-bag-512 (2)", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap nguoisudung {
+            get {
+                object obj = ResourceManager.GetObject("nguoisudung", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -373,9 +443,9 @@ namespace QuanLyThuChi.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap search {
+        internal static System.Drawing.Bitmap save {
             get {
-                object obj = ResourceManager.GetObject("search", resourceCulture);
+                object obj = ResourceManager.GetObject("save", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
@@ -386,36 +456,6 @@ namespace QuanLyThuChi.Properties {
         internal static System.Drawing.Bitmap secrecy_icon {
             get {
                 object obj = ResourceManager.GetObject("secrecy-icon", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Technical_Support_512 {
-            get {
-                object obj = ResourceManager.GetObject("Technical_Support-512", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Technical_Support_512__1_ {
-            get {
-                object obj = ResourceManager.GetObject("Technical_Support-512 (1)", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap them {
-            get {
-                object obj = ResourceManager.GetObject("them", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
