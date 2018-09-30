fe64d4a85eddbb2213d104e7c472035872bb8cb9
thanhkhoatran <35222306+thanhkhoatran@users.noreply.github.com>
2018-09-30 15:47:54 +0700

    frm_PT

---
+++ QuanLyThuChi/QuanLyThuChi/frm_PhieuThu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using DevExpress.UserSkins;
+using DevExpress.Skins;
+namespace QuanLyThuChi
+{
+    public partial class frm_PhieuThu : DevExpress.XtraEditors.XtraForm
+    {
+        public frm_PhieuThu()
+        {
+            InitializeComponent();
+            DevExpress.UserSkins.BonusSkins.Register();
+        }
+
+       
+    }
+}
